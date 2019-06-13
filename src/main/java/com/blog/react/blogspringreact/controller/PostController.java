d403864be259f1fce431815dce684e38e776aea9
teten777 <teten.nugraha18@gmail.com>
2019-06-13 14:58:56 +0700

    get posts

--- src/main/java/com/blog/react/blogspringreact/controller/PostController.java
+++ src/main/java/com/blog/react/blogspringreact/controller/PostController.java
@@ -1,5 +1,11 @@
 package com.blog.react.blogspringreact.controller;
 
+import com.blog.react.blogspringreact.response.SuccessResponse;
+import com.blog.react.blogspringreact.service.PostService;
+import com.blog.react.blogspringreact.util.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/api/posts")
 public class PostController {
 
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAll() {
+
+        Iterable posts = postService.findAll();
+
+        return SuccessResponse.response(
+                posts,
+                Status.GET_ALL,
+                ""
+        );
+
+    }
 
 
 }
