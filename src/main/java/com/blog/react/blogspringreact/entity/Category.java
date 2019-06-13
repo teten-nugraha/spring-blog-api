e4105e3e9286dc06c2be70a2326576f553afea3e
teten777 <teten.nugraha18@gmail.com>
2019-06-13 11:05:25 +0700

    Add Category Repository

--- src/main/java/com/blog/react/blogspringreact/entity/Category.java
+++ src/main/java/com/blog/react/blogspringreact/entity/Category.java
@@ -1,4 +1,4 @@
-package com.blog.react.blogspringreact;
+package com.blog.react.blogspringreact.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
---
+++ src/main/java/com/blog/react/blogspringreact/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.blog.react.blogspringreact.repository;
+
+import com.blog.react.blogspringreact.entity.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
