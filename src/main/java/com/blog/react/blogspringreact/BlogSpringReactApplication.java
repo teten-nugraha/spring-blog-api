7dd3f65e783d007a358822762717aa5d51ecf28d
teten777 <teten.nugraha18@gmail.com>
2019-06-19 14:50:52 +0700

    user service update

--- src/main/java/com/blog/react/blogspringreact/BlogSpringReactApplication.java
+++ src/main/java/com/blog/react/blogspringreact/BlogSpringReactApplication.java
@@ -3,9 +3,16 @@ package com.blog.react.blogspringreact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BlogSpringReactApplication {
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BlogSpringReactApplication.class, args);
---
+++ src/main/java/com/blog/react/blogspringreact/service/UserService.java
@@ -0,0 +1,34 @@
+package com.blog.react.blogspringreact.service;
+
+import com.blog.react.blogspringreact.entity.User;
+import com.blog.react.blogspringreact.exception.UsernameAlreadyExistsException;
+import com.blog.react.blogspringreact.repository.UserRepository;
+import org.apache.catalina.realm.UserDatabaseRealm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser) {
+        try{
+
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            newUser.setUsername(newUser.getUsername());
+
+            return userRepository.save(newUser);
+
+        }catch (Exception e) {
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+        }
+    }
+
+
+}
