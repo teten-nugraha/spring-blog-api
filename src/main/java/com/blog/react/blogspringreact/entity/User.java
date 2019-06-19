7594407a54925c289a8234d43a9c16cf41813e21
teten777 <teten.nugraha18@gmail.com>
2019-06-19 14:34:18 +0700

    add user entity

---
+++ src/main/java/com/blog/react/blogspringreact/entity/User.java
@@ -0,0 +1,66 @@
+package com.blog.react.blogspringreact.entity;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+@Data
+@ToString
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Username needs to be an email")
+    @NotBlank(message = "Username is required")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "fullname is required")
+    private String fullname;
+
+    @NotBlank(message = "password is required")
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+    private Date created_At;
+    private Date updated_At;
+
+    public User() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
