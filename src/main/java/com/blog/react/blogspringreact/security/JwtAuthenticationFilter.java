57b1dc84ec3c2310131317afbdd1d365e3b5f80e
teten777 <teten.nugraha18@gmail.com>
2019-06-19 16:07:56 +0700

    jwt auth filter

--- src/main/java/com/blog/react/blogspringreact/security/JwtAuthenticationFilter.java
+++ src/main/java/com/blog/react/blogspringreact/security/JwtAuthenticationFilter.java
@@ -1,6 +1,12 @@
 package com.blog.react.blogspringreact.security;
 
+import com.blog.react.blogspringreact.entity.User;
+import com.blog.react.blogspringreact.service.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -8,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 
 /**
  * untuk mengakses endpoint yang sudah di secure
@@ -17,9 +24,42 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtTokenProvider tokenProvider;
 
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String jwt = getJwtFromRequest(httpServletRequest);
+
+            if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+                Long userId = tokenProvider.getUserIdFromJWT(jwt);
+                User userDetails = customUserDetailService.loadUserById(userId);
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, Collections.emptyList());
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            }
+        } catch (Exception ex) {
+            logger.error("Could not set user authentication in securty context ", ex);
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest httpServletRequest) {
+        String bearerToken = httpServletRequest.getHeader(SecurityConstants.HEADER_STRING);
+
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+
+        return null;
 
     }
 }
