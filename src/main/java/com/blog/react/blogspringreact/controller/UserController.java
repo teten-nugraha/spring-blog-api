649723d422139b87ddfeec673c329b6efae8e82b
teten777 <teten.nugraha18@gmail.com>
2019-06-19 14:53:28 +0700

    create usercontroller

---
+++ src/main/java/com/blog/react/blogspringreact/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.blog.react.blogspringreact.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api/users")
+public class UserController {
+
+    
+
+}
