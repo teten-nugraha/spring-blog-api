7080ae29b781a9a54af73d2539515f71d8238e4a
teten777 <teten.nugraha18@gmail.com>
2019-06-19 14:40:58 +0700

    user repository

---
+++ src/main/java/com/blog/react/blogspringreact/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.blog.react.blogspringreact.repository;
+
+import com.blog.react.blogspringreact.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+    User getById(Long id);
+
+}
