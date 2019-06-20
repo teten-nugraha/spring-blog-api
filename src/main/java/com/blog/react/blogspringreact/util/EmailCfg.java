03cc9e72a113a00e3531c05898271cd5d964c47a
teten777 <teten.nugraha18@gmail.com>
2019-06-20 16:47:09 +0700

    email cfg

---
+++ src/main/java/com/blog/react/blogspringreact/util/EmailCfg.java
@@ -0,0 +1,23 @@
+package com.blog.react.blogspringreact.util;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class EmailCfg {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+}
