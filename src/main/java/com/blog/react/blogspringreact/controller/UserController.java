626d556b7ce8e8fdd00fe34662e6920cb5618e9e
teten777 <teten.nugraha18@gmail.com>
2019-06-20 17:06:18 +0700

    sent email

--- src/main/java/com/blog/react/blogspringreact/controller/UserController.java
+++ src/main/java/com/blog/react/blogspringreact/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.blog.react.blogspringreact.controller;
 
+import com.blog.react.blogspringreact.entity.Feedback;
 import com.blog.react.blogspringreact.entity.User;
 import com.blog.react.blogspringreact.payload.JWTLoginSuccessResponse;
 import com.blog.react.blogspringreact.payload.LoginRequest;
@@ -8,11 +9,14 @@ import com.blog.react.blogspringreact.security.JwtTokenProvider;
 import com.blog.react.blogspringreact.security.SecurityConstants;
 import com.blog.react.blogspringreact.service.MapValidationErrorService;
 import com.blog.react.blogspringreact.service.UserService;
+import com.blog.react.blogspringreact.util.EmailCfg;
 import com.blog.react.blogspringreact.util.Status;
 import com.blog.react.blogspringreact.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -24,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.ValidationException;
 
 @RestController
 @RequestMapping("/v1/api/users")
@@ -43,6 +48,9 @@ public class UserController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private EmailCfg emailCfg;
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
@@ -74,6 +82,43 @@ public class UserController {
         User newUser = userService.saveUser(user);
 
         return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/feedback")
+    public ResponseEntity<?> feedback(@RequestBody Feedback feedback, BindingResult result) {
+
+        if(result.hasErrors()){
+            throw new ValidationException("Invalid");
+        }
+
+        try{
+            // Create a mail sender
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost(this.emailCfg.getHost());
+            mailSender.setPort(this.emailCfg.getPort());
+            mailSender.setUsername(this.emailCfg.getUsername());
+            mailSender.setPassword(this.emailCfg.getPassword());
+            // Create an email instance
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(feedback.getEmail());
+            mailMessage.setTo("teten@hackflix.com");
+            mailMessage.setSubject("New feedback from " + feedback.getName());
+            mailMessage.setText(feedback.getFeedback());
+            // Send mail
+            mailSender.send(mailMessage);
+
+            return SuccessResponse.response(
+              feedback,
+                    Status.SUCCESS,
+                    "Email Anda sudah terkirim  "
+            );
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
     }
 
 }
